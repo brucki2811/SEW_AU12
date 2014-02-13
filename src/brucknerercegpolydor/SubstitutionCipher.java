3e87753eb8d2a7b42f721dc657bcccf50dfc8317
steff <stef1997@gmail.com>
2014-02-13 11:20:16 +0100

    addes LowerCase for encrypt

--- src/brucknerercegpolydor/SubstitutionCipher.java
+++ src/brucknerercegpolydor/SubstitutionCipher.java
@@ -11,10 +11,10 @@ public class SubstitutionCipher extends MonoAlphabeticCipher {
 	public SubstitutionCipher(String secretalphabet) {
 		this.setSecretAlphabet(secretalphabet);
 	}
-	 
+	
 	public void setSecretAlphabet(String secretAlphabet) {
 		this.setSecretAlphabet(secretAlphabet);
 	}
-	 
+	
 }
  
