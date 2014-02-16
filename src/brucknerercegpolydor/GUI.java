19e64181df3e114e53b3fa0ee73adafad57bf1bd
Michael Bruckner <mbruckner@student.tgm.ac.at>
2014-02-16 14:11:24 +0100

    Hab eine kleinigkeit weitergearbeitet.

--- src/brucknerercegpolydor/GUI.java
+++ src/brucknerercegpolydor/GUI.java
@@ -10,15 +10,16 @@ public class GUI {
 	@SuppressWarnings("unused")
 	private ShiftCipher sc;
 	@SuppressWarnings("unused")
-	private MonoalphabeticCipher mac;
+	private MonoAlphabeticCipher mac;
 	@SuppressWarnings("unused")
-	private SubstitutionsCipher sbc;
+	private SubstitutionCipher sbc;
 	@SuppressWarnings("unused")
 	private KeywordCipher kc;
 	
 	
 	private JFrame f;
-	private JTextArea ta;
+	private JTextArea ta1;
+	private JTextArea ta2;
 	private JButton jb1;
 	private JButton jb2;
 	private JLabel jl;
@@ -26,7 +27,7 @@ public class GUI {
 	private Container button;
 	private Container text;
 	
-	public GUI(Cipher c,ShiftCipher sc,MonoalphabeticCipher mac,SubstitutionsCipher sbc,KeywordCipher kc){
+	public GUI(){
 		
 		this.c = c;
 		this.sc = sc;
@@ -34,8 +35,9 @@ public class GUI {
 		this.sbc = sbc;
 		this.kc = kc;
 		
-		this.f = new JFrame("Verschlüsselung");
-		this.ta = new JTextArea();
+		this.f = new JFrame("Verschlüsselung/Entschlüsselung");
+		this.ta1 = new JTextArea();
+		this.ta2 = new JTextArea();
 		this.jb1 = new JButton("Verschlüsseln");
 		this.jb2 = new JButton("Entschlüsseln");
 		this.jl = new JLabel();
@@ -44,12 +46,35 @@ public class GUI {
 		this.button = new Container();
 		this.text = new Container();
 		
+		this.f.setLayout(new BorderLayout());
+		this.button.setLayout(new GridLayout(0,2));
+		this.text.setLayout(new GridLayout(0,2));
+		
+		this.button.add(this.jb1);
+		this.button.add(this.jb2);
+		this.text.add(this.ta1, BorderLayout.WEST);
+		this.text.add(this.ta2, BorderLayout.EAST);
+		
+		
+		this.f.add(this.button, BorderLayout.SOUTH);
+		this.f.add(this.text);
+		
+		
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(500, 500);
         f.setVisible(true);
 	}
-	public String getText() {
-		return ta.getText();
+	public String getText1() {
+		return ta1.getText();
+	}
+	public String getText2() {
+		return ta2.getText();
+	}
+	public void setText1(String text1) {
+		ta1.setText(text1);
+	}
+	public void setText2(String text2) {
+		ta2.setText(text2);
 	}
 	public boolean getButtonB1(ActionEvent e){
         if(e.getSource() == this.jb1)return true;
@@ -59,7 +84,5 @@ public class GUI {
         if(e.getSource() == this.jb2)return true;
         return false;
     }
-	public void main (String[]args){
-		
-	}
+
 }
