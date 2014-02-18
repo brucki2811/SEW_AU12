95ccd584f2cdb277318df77bfcbb2b37f167cce0
Michael Bruckner <mbruckner@student.tgm.ac.at>
2014-02-18 12:21:19 +0100

    added Iterator + ArrayList

--- src/brucknerercegpolydor/StartCipher.java
+++ src/brucknerercegpolydor/StartCipher.java
@@ -6,12 +6,14 @@ package brucknerercegpolydor;
  */
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.util.Iterator;
+import java.util.ArrayList;
 
 public class StartCipher implements ActionListener, ListSelectionListener {
 	private StartCipher stc;
@@ -34,11 +36,12 @@ public class StartCipher implements ActionListener, ListSelectionListener {
 	private JButton jb2;
 	private JLabel l;
 	private JList<String> jl;
+	private DefaultListModel<String> dlm;
+	
+	private ArrayList<Cipher> cipher;
 	
 	private Container button;
 	private Container text;
-	
-
 
 	public StartCipher(){
 
@@ -49,12 +52,16 @@ public class StartCipher implements ActionListener, ListSelectionListener {
 		this.ta2 = new JTextArea();
 		this.jb1 = new JButton("Verschlüsseln");
 		this.jb2 = new JButton("Entschlüsseln");
-
+		
+		cipher = new ArrayList<Cipher>();
+		
+		this.dlm = new DefaultListModel<String>();
 		this.l = new JLabel();
 		this.jl = new JList<String>();
 		this.jl.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		this.jl.setBorder(BorderFactory.createEtchedBorder(Color.white, Color.gray));
 		this.jl.addListSelectionListener(stc);
+		
 		
 		this.button = new Container();
 		this.text = new Container();
@@ -62,14 +69,13 @@ public class StartCipher implements ActionListener, ListSelectionListener {
 		this.f.setLayout(new BorderLayout());
 		this.button.setLayout(new GridLayout(0,2));
 		this.text.setLayout(new GridLayout(0,3));
-		
+				
 		this.button.add(this.jb1);
 		this.button.add(this.jb2);
 		
 		this.text.add(this.ta1, BorderLayout.WEST);
-		this.text.add(this.l, BorderLayout.CENTER);
+		this.text.add(this.jl,BorderLayout.CENTER);
 		this.text.add(this.ta2, BorderLayout.EAST);
-		
 		
 		this.f.add(this.button, BorderLayout.SOUTH);
 		this.f.add(this.text);
@@ -84,26 +90,40 @@ public class StartCipher implements ActionListener, ListSelectionListener {
 	public String getText1() {
 		return ta1.getText();
 	}
+	
+	
 	public String getText2() {
 		return ta2.getText();
 	}
-	public void setText1(String text1) {
-		ta1.setText(text1);
+	
+	
+	public void setText1(String text) {
+		ta1.setText(text);
 	}
+	
+	
 	public void setText2(String text2) {
 		ta2.setText(text2);
+	}
+	
+	public void setTitel(){
+		
+	}
+	public void getTitel(){
+		
 	}
 	public boolean getButtonB1(ActionEvent e){
         if(e.getSource() == this.jb1)return true;
         return false;
     }
+	
+	
 	public boolean getButtonB2(ActionEvent e){
         if(e.getSource() == this.jb2)return true;
         return false;
     }
-	public static void main (String[]args){
-		StartCipher stc = new StartCipher();
-	}
+	
+	
 	public void actionPerformed(ActionEvent e){
 		if(this.stc.getButtonB1(e)){
 			this.stc.setText1(stc.getText2());
@@ -111,5 +131,35 @@ public class StartCipher implements ActionListener, ListSelectionListener {
 		if(this.stc.getButtonB2(e)){
 			this.stc.setText2(stc.getText1());
 		}
+	}
+	
+	
+	public int getSelectedIndex() {
+		return jl.getSelectedIndex();
+	}
+	
+	
+	public void addCipher(String titel){
+		this.dlm.addElement("Substitution Cipher");
+		
+	}
+	
+	public void allCiphers(){
+		int i= 0;
+		Iterator<Cipher> it = cipher.iterator();
+		while (it.hasNext()){
+			Cipher cipher = it.next();
+			System.out.println(i + ": " + cipher);
+		}
+	}
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		
+	}
+	
+	
+	public static void main (String[]args){
+		StartCipher stc = new StartCipher();
+		
 	}
 }
