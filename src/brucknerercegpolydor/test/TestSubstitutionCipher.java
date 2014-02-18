d03d9172e32d9e12f4ccde3a2132e3bc6de7ae5e
Stipe <Stipe@ERCEG>
2014-02-18 10:05:20 +0100

    added test-classes

---
+++ src/brucknerercegpolydor/test/TestSubstitutionCipher.java
@@ -0,0 +1,14 @@
+package brucknerercegpolydor.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSubstitutionCipher {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
